1b89042de9cfe0f4f5b7f3f479864b9df5bd3bd3
Lahiru66 <lahirukasun666@gmail.com>
2024-09-15 11:36:49 +0530

    adding post request and assertions

--- src/test/java/Sample.java
+++ src/test/java/Sample.java
@@ -1,6 +1,7 @@
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
@@ -14,15 +15,19 @@ public class Sample {
 
     public static Response response;
 
-    @BeforeMethod
+    private static String requestBody = "{\n" +
+            "  \"title\": \"foo\",\n" +
+            "  \"body\": \"bar\",\n" +
+            "  \"userId\": \"1\" \n}";
+
+    @BeforeMethod(alwaysRun = true)
     public void setUp() {
         softAssert = new SoftAssert();
+        RestAssured.baseURI = "https://jsonplaceholder.typicode.com";
     }
 
-    @Test
+    @Test(enabled = true, priority = 1, description = "get one post")
     public void testGetExampleAPI() {
-        // Define the base URI where the API is hosted
-        RestAssured.baseURI = "https://jsonplaceholder.typicode.com";
 
         response = given()
                 .contentType("application/json")
@@ -46,6 +51,35 @@ public class Sample {
         softAssert.assertEquals(responseBody.contains("id"), true, "Response body does not contain 'id'");
         softAssert.assertEquals(responseBody.contains("title"), true, "Response body does not contain 'title'");
         softAssert.assertEquals(responseBody.contains("body"), true, "Response body does not contain 'body'");
+    }
+
+
+    @Test(enabled = true, priority = 2, description = "create posts")
+    public void postRequest() {
+        Response response = given()
+                .header("Content-type", "application/json")
+                .and()
+                .body(requestBody)
+                .when()
+                .post("/posts")
+                .then()
+                .extract().response();
+
+        // Retrieve the status code from the response
+        int statusCode = response.getStatusCode();
+
+        // Assert that the status code is 200 (OK)
+        assertEquals(statusCode, 201, "Status code is not 200");
+
+        softAssert.assertEquals("foo", response.jsonPath().getString("title"));
+        softAssert.assertEquals("bar", response.jsonPath().getString("body"));
+        softAssert.assertEquals("1", response.jsonPath().getString("userId"));
+        softAssert.assertEquals("101", response.jsonPath().getString("id"));
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void cleanup() {
+        response = null;
     }
 
 
