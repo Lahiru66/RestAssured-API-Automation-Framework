b71a37e88d83c5f0898696ed02425d35067845c7
Lahiru66 <lahirukasun666@gmail.com>
2024-09-28 10:33:09 +0530

    adding common validations class

---
+++ src/main/java/utils/CommonValidations.java
@@ -0,0 +1,13 @@
+package utils;
+
+import org.testng.annotations.Test;
+import static org.testng.Assert.assertEquals;
+
+public class CommonValidations {
+
+    public static void verifyResponseCode(int actual, int expected){
+        assertEquals(actual,expected,"assertion failed");
+    }
+}
+
+
