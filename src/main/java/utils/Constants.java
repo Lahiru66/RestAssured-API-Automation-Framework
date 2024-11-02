ec7f5982a360b5465e41eabb2f9c4b697ffafc6c
Lahiru66 <lahirukasun666@gmail.com>
2024-11-02 16:37:24 +0530

    Adding Constants and StatusCodes files

---
+++ src/main/java/utils/Constants.java
@@ -0,0 +1,11 @@
+package utils;
+
+public class Constants {
+
+    public static final String TITLE = "title";
+    public static final String BODY = "body";
+    public static final String USERID = "userId";
+
+    public static final String ID = "id";
+
+}
---
+++ src/main/java/utils/StatusCodes.java
@@ -0,0 +1,23 @@
+package utils;
+
+public class StatusCodes {
+    public int SC_OK = 200;
+    public int SC_CREATED = 201;
+
+    public int SC_NO_CONTENT = 204;
+
+    public int SC_MOVED_PERMANENTLY = 301;
+
+    public int SC_BAD_REQUEST = 400;
+
+    public int SC_UNAUTHORIZED = 401;
+
+    public int SC_NOT_FOUND = 404;
+
+    public int SC_INTERNAL_SERVER_ERROR = 500;
+
+    public int SC_NOT_IMPLEMENTED = 501;
+
+    public int SC_BAD_GATEWAY = 502;
+
+}
